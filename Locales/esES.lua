5f1fb2d6980c123065a5efcc21f12d125e2b71d3
Halfway There <67455065+halfwaythere1@users.noreply.github.com>
2025-03-19 20:50:39 -0400

    Add files via upload

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -1,4 +1,4 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("WhisperUnfriendly", "esES")
+local L = LibStub("AceLocale-3.0"):NewLocale("WhisperUnfriendly", "esES") or LibStub("AceLocale-3.0"):NewLocale("WhisperUnfriendly", "esMX")
 
 if not L then return end
 
